bf5ee1e8dc42b28a876ae8549a20ceef6b1a98c0
tart3el7antash <22310085@scme.edu.ps>
2024-11-03 10:00:29 +0200

    Create list.py

---
+++ 03. Data Structures/listTartel/list.py
@@ -0,0 +1,17 @@
+list1 = [2, 3, 4]
+print(len(list1))
+print(max(list1))
+print(min(list1))
+print(sum(list1))
+import random
+random.shuffle(list1)
+print(list1)
+list2 =[1, 9, 5]
+list3 = list1 + list2
+print(list3)
+list3 = 4*list1
+print(list3)
+list1 [-1]
+print(list1)
+list3 [-4]
+print(list3)
